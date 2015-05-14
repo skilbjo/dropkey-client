6ae3b768e6df19ab45f03410295df3b471a9ceed
John Skilbeck <john.skilbeck@gmail.com>
2015-05-13 19:25:36 -0700

    revisiting

--- app.js
+++ app.js
@@ -1,76 +1,43 @@
-// https://www.npmjs.com/package/usb-detection
-// https://github.com/voodootikigod/node-serialport
-// https://github.com/sidorares/node-dbus
+var njds 		= require('nodejs-disks'),
+	usb 			= require('usb'),
+	args 			= process.argv.slice(2),
+	fs 				= require('fs'),
+	path 			= require('path'),
+	dbox 			= require('dbox'),
+	token 		= require('./config.js').dropbox_token,
+	avaiable_drives = []
+	;
 
-var njds = require('nodejs-disks');
-
-njds.drives(function(err, drives) {
-	njds.drivesDetail(drives, function(err, drive) {
-		drive.forEach(function(drive) {
-			console.log(drive.mountpoint);
+var getDrives = function(cb) {
+	njds.drives(function(err, drives) {
+		njds.drivesDetail(drives, function(err, drive) {
+			drive.forEach(function(drive) {
+				return avaiable_drives.push(drive.mountpoint);
+			});
+			cb();
 		});
 	});
+};
+
+getDrives(function() {
+	console.log(avaiable_drives[1]);
+	readDerp();
 });
 
-// var dbus = require('dbus-native');
-// var sessionBus = dbus.sessionBus();
-// sessionBus.getService('org.freedesktop.Notifications').getInterface(
-//     '/org/freedesktop/Notifications',
-//     'org.freedesktop.Notifications', function(err, notifications) {
+var readDerp = function() {
+	var drp_file = path.join(avaiable_drives[1], 'md5.drp');
+	fs.readFile(drp_file, 'utf-8', function(err, data) {
+		console.log(data);
+	});
+};
 
-//     // dbus signals are EventEmitter events
-//     notifications.on('ActionInvoked', function() {
-//         console.log('ActionInvoked', arguments);
-//     });
-//     notifications.on('NotificationClosed', function() {
-//         console.log('NotificationClosed', arguments);
-//     });
-//     notifications.Notify('exampl', 0, '', 'summary 3', 'new message text', ['xxx yyy', 'test2', 'test3', 'test4'], [],  5, function(err, id) {
-//        //setTimeout(function() { n.CloseNotification(id, console.log); }, 4000);
-//     });
-// });
+console.log(token);
 
 
-// var dbus = require('dbus-native');
-// var conn = dbus.createConnection();
-// conn.message({
-//     path:'/org/freedesktop/DBus',
-//     destination: 'org.freedesktop.DBus',
-//     'interface': 'org.freedesktop.DBus',
-//     member: 'Hello',
-//     type: dbus.messageType.methodCall
-// });
-// conn.on('message', function(msg) { console.log(msg); });
-
-// var usb = require('usb'),
-// 	connected = usb.getDeviceList(),
-// 	device1 	= connected[0],
-// 	args 			= process.argv.slice(2)
-// 	;
 
 
-// 	console.log(
-// 		JSON.stringify(connected[0])
-// 	);
+// ask user to provide oauth details for dropbox
+// create .drp file
+// pull a file from dropbox using authentication from .drp file
 
-// 	console.log(
-// 		device1.portNumbers
-// 		);
 
-// console.log(
-// 	device1.interfaces
-// );
-
-// var dbus = require('dbus-native');
-// var bus = dbus.systemBus();
-
-// udservice = bus.getService('org.freedesktop.UDisks');
-// udservice.getInterface(
-//     '/org/freedesktop/UDisks',
-//     'org.freedesktop.UDisks',
-//     function(err, ud) {
-//       ud.on('DeviceAdded', function(deviceObjectPath) {
-//         console.log('DeviceAdded', deviceObjectPath);
-//       });
-//     }
-//   );
